1ab33532865c9395d6957b7a999b14a4cb91d7cb
1Elliot1 <eoconnor1@ithaca.edu>
2025-02-13 11:54:49 -0500

    Refactored all previous code into a PanoramaData class to avoid redundant/unclear/inefficient code

--- firewallRuleLookupSystem.py
+++ firewallRuleLookupSystem.py
@@ -9,7 +9,205 @@ from dotenv import load_dotenv
 import ipaddress
 import os
 
+class PanoramaData:
+    """
+    Encapsulates retrieval and correlation of Panorama data. 
+    Builds lookup maps for 
+        AddressObjects
+        DeviceGroups
+        AddressGroups
+        VLANs
+        Zones
+        Interfaces
+    """
+    def __init__(self, pano):
+        self.pano = pano
+        self.addressObjects = AddressObject.refreshall(pano)
+        self.addressGroups = AddressGroup.refreshall(pano)
+        self.deviceGroups = DeviceGroup.refreshall(pano)
+        self.templates = Template.refreshall(pano)
+
+        self.deviceGroupRules = {}
+        self.vlanData = {}
+
+        self.collectDeviceGroupRules()
+        self.collectVlanData()
+
+    def collectDeviceGroupRules(self):
+        """
+        Refresh rules for each device group
+        """
+        for dg in self.deviceGroups:
+            logging.info(f"Retrieving Device Group '{dg.name} Rules")
+            self.deviceGroupRules[dg.name] = fetchAllPreRulebaseRules(dg)
+
+    def fetchAllPrerulebaseRules(self, deviceGroup):
+        """
+        Return a dictionary of rule types for the given device group
+        """
+        #Still cannot get QoS, DoS, NetworkPacketBroker, TunnelInspection, or SD-WAN rules (not in panos.policies)
+        ruleTypes = [
+            SecurityRule,
+            NatRule, 
+            ApplicationOverride,
+            PolicyBasedForwarding,
+            DecryptionRule,
+            AuthenticationRule
+        ]
+
+        #define prerulebase per device group
+        prerulebase = deviceGroup.find(PreRulebase)
+        if prerulebase is None:
+            prerulebase = PreRulebase()
+            deviceGroup.add(prerulebase)
+        allRules = {}
+        #for each ruletype in devices prerulebase
+        for ruleType in ruleTypes:
+            ruleName = ruleType.__name__
+            #refresh the ruletypes prerulebase
+            rules = ruleType.refreshall(prerulebase)
+            #add an entry to allRules that has ruleType.name as key, with refresshed rules as values
+            allRules[ruleName] = rules
+        return allRules
+
+    def collectVlanData(self):
+        """
+        For every template (and its vsys), grab associated zones and VLAN mappings
+        Using aggregate interfaces and their subinterfaces
+        """
+        for template in self.tempaltes:
+            vsysList = template.findall(Vsys)
+            for vsys in vsysList:
+                zones = vsys.findall(Zone)
+                aggInterfaces = template.findall(AggregateInterface)
+                for aggInterface in aggInterfaces:
+                    subInterface = self.getChildrenOfAggInterface(aggInterface)
+                    vlanMap = self.getAddressForVLANS(subInterface)
+                    
+                    key = f"{template.name}-{vsys.name}"
+                    if key not in self.vlanData:
+                        self.vlanData[key] = {"vlanMap": vlanMap, "zones": zones}
+                    else:
+                        self.vlanData[key]["vlanMap"].update(vlanMap)
+
+    @staticmethod
+    def getChildrenOfAggInterface(aggInterface):
+        """
+        Return subinterfaces for an aggregate interface. 
+        TODO: currently only supporting Layer3Subinterfaces
+        """
+        return aggInterface.findall(Layer3Subinterface)
+
+    @staticmethod
+    def getAddressForVLANS(subinterfaceList):
+        """
+        Create mapping of VLAN numbers ot their associated IP ranges
+        Assumes that the second half of subinterface name, preceeded by a period is vlan num
+        """
+        interfaceMap = {}
+        for subinterface in subinterfaceList:
+            try:
+                vlanNum = subinterface.name.split(".")[1]
+                interfaceMap[vlanNum] = subinterface.ip
+            except IndexError:
+                logging.error(f"Error parsing VLAN number from subinterface name: {subinterface.name}")
+        return interfaceMap
+
+    @staticmethod
+    def ipInCidr(ip, cidr):
+        """
+        Check if an IP address is within a CIDR range
+        """
+        try:
+            return ipaddress.ip_address(ip) in ipaddress.ip_network(cidr, strict = False)
+        except ValueError:
+            return False
+
+    def correlateAddressToVlan(self, addressObject, vlanMap):
+        """
+        Given an address object and VLAN map (VlanNum -> cidr), return the VLAN number if found
+        """
+        for vlanNum, vlanCidr in vlanMap.items():
+            if self.ipInCidr(addressObject.value, vlanCidr):
+                return vlanNum
+        return None
+    
+    @staticmethod
+    def correlateVlanToZone(vlanNum, zones):
+        """
+        Given a VLAN number and a list of zones, try to correlate VLAN to a zone
+        TODO: GRAB VLAN NAMES AND INTERFACES CORRECTLY 
+        """
+        for zone in zones:
+            zoneInterfaces = zone.findall(Interface)
+            for interface in zoneInterfaces:
+                try:
+                    interfaceVlan = interface.name.split(".")[1]
+                    if interfaceVlan == vlanNum:
+                        return zone.name
+                except IndexError:
+                    logging.error(f"Error parsing VLAN number from interface name: {interface.name}")
+                    continue 
+        return None
+
+    def correlateAddressToAddressGroup(self, addressObject):
+        """
+        Given an address object, try to correlate it to an address group
+        """
+        for addressGroup in self.addressGroups:
+            if hasattr(addressGroup, "static_value"):
+                if any(addressObject.name == a or addressObject.value == a for a in addressGroup.static_value):
+                    return addressGroup.name
+        return None
+
+    def correlateIP(self, ip):
+        """
+        given an IP address (String), find its associated AddressObject, VLAN, Zone, AddressGroup, and DeviceGroup rules
+        """
+        #find matching addressObject for the given IP
+        matchedObject = next((obj for obj in self.addressObjects if obj.value == ip), None)
+        if not matchedObject:
+            logging.error(f"No AddressObject found for IP: {ip}")
+            return None
+        
+        correlationResult = {
+            "ip": ip,
+            "addressObject": matchedObject.name,
+            "vlan": None,
+            "zone": None,
+            "addressGroup": None,
+            "deviceGroupRules": {} #can be further filtered based on context
+        }
+
+        #check each vlanData bucket until a matching VLAN is found
+        for key, data in self.vlanData.items():
+            vlanMap = data.get("vlanMap", {})
+            zones = data.get("zones", [])
+            vlanNum = self.correlateAddressToVlan(matchedObject, vlanMap)
+            if vlanNum:
+                correlationResult["vlan"] = vlanNum
+                zoneName = self.correlateVlanToZone(vlanNum, zones)
+                correlationResult["zone"] = zoneName
+                break #stop searching once a match is found
+
+        #Correlate the AddressObject to an AddressGroup
+        addressGroup = self.correlateAddressToAddressGroup(matchedObject)
+        if addressGroup:
+            correlationResult["addressGroup"] = addressGroup
+        
+        #Device group rules can be refined further
+        #For now,including all device group rules as starting point
+        #TODO: Figure out what you need to do with that
+        correlationResult["deviceGroupRules"] = self.deviceGroupRules
+
+        return correlationResult
+
+
 def main():
+    #Address Objects are missing 11 total objects, because they are held in a different location
+    #than the rest of them. A couple in IC-Perimeter and the rest in IC-Datacenter.
+    #TODO: Find out how to access the objects in these non-default locations
+
     load_dotenv()
     apiKey = os.environ.get('API_KEY')
     panAddress = os.environ.get('PAN_ADDRESS')
@@ -23,138 +221,17 @@ def main():
     logging.info("Refreshing Managed Devices...")
     devices = pano.refresh_devices()
     logging.info("Found %d Managed Devices", len(devices))
-    #get all address objects
-    addressObject = AddressObject.refreshall(pano)
-    #get all address groups
-    addressGroup = AddressGroup.refreshall(pano)
-    fetchDeviceGroupInfo(pano)
 
-    #Address Objects are missing 11 total objects, because they are held in a different location
-    #than the rest of them. A couple in IC-Perimeter and the rest in IC-Datacenter.
-    #TODO: Find out how to access the objects in these non-default locations
+    #build PanoramaData object
+    panData = PanoramaData(pano)
 
+    #Test Example:
+    testIP = ""
+    result = panData.correlateIP(testIP)
+    if result:
+        logging.info(f"Correlation Result for IP: {testIP}\n{result}")
+    else:
+        logging.error(f"No correlation result found for IP: {testIP}")
 
-
-def fetchDeviceGroupInfo(pano):
-    #Find and refresh all dev groups
-    logging.info("Retrieving and Refreshing Device Groups...")
-    deviceGroups = DeviceGroup.refreshall(pano)
-    ruleMap = {}
-    for dg in deviceGroups: 
-        logging.info(f"Retrieving Device Group '{dg.name}' Rules")
-        ruleMap[dg.name] = fetchAllPreRulebaseRules(dg)
-    
-
-
-def fetchTemplateInfo(pano):
-    logging.info("Retrieving and Refreshing Templates")
-    #get all templates
-    templates = Template.refreshall(pano)
-    print(templates)
-
-    for template in templates:
-        #Get all vsys' per template
-        vsysList = template.findall(Vsys)
-        zones = []
-        for vsys in vsysList:
-            #Find all zones per vsys
-            zones = vsys.findall(Zone)
-            if zones:
-                logging.info(f"     {len(zones)} Zones Found For Template '{template}, Vsys '{vsys}'")
-            else:
-                logging.info(f"No Zones found for template '{template}', Vsys '{vsys}'")
-
-        #Find all interfaces. Currently aggInterfaces show up as the parent agg
-        ethInterfaces = template.findall(EthernetInterface)
-        aggInterfaces = template.findall(AggregateInterface)
-        if ethInterfaces:
-            logging.info(f"     {len(ethInterfaces)} Ethernet Interfaces Found for Template '{template}', Vsys '{vsys}'")
-        else:
-            logging.info(f"No Ethernet Interfaces Found for Template {template}, Vsys '{vsys}'")
-        if aggInterfaces:
-            logging.info(f"     {len(aggInterfaces)} Aggregate Interfaces Found for Template '{template}', Vsys '{vsys}'")
-        else:
-            logging.info(f"No Aggregate Interfaces Found for Template '{template}', Vsys '{vsys}'")
-        for aggInterface in aggInterfaces:
-            subInterfaces = getChildrenOfAggInterface(aggInterface)
-            vlanMap = getAddressForVLANS(subInterfaces)
-        #Do this for every addressObject 
-            addressObjectVlanNum = correlateAddressToVLAN(addressObject, vlanMap)
-            addressObjectZone = correlateVLANToZone(zones, addressObjectVlanNum)
-            addressObjectAddressGroup = correlateAddressToAddressGroup(addressObject, addressGroups)
-            print(f"Address Object: {addressObject.name} is in VLAN {addressObjectVlanNum}, Zone {addressObjectZone}, and Address Group {addressObjectAddressGroup}")
-
-def fetchAllPreRulebaseRules(deviceGroup):
-    #Still cannot get QoS, DoS, NetworkPacketBroker, TunnelInspection, or SD-WAN rules (not in panos.policies)
-    ruleTypes = [
-        SecurityRule,
-        NatRule, 
-        ApplicationOverride,
-        PolicyBasedForwarding,
-        DecryptionRule,
-        AuthenticationRule
-    ]
-
-    #define prerulebase per device group
-    prerulebase = deviceGroup.find(PreRulebase)
-    if prerulebase is None:
-        prerulebase = PreRulebase()
-        deviceGroup.add(prerulebase)
-    
-    allRules = {}
-    #for each ruletype in devices prerulebase
-    for ruleType in ruleTypes:
-        ruleName = ruleType.__name__
-        #refresh the ruletypes prerulebase
-        rules = ruleType.refreshall(prerulebase)
-        #add an entry to allRules that has ruleType.name as key, with refresshed rules as values
-        allRules[ruleName] = rules
-    return allRules
-
-#TODO: Logic for correlating rules to their respective zones, interfaces, and address objects
-#now have a dictionary of all rules per device group, need to correlate them next
-
-#TODO get the VLAN, AddressGroup, Zone, and Interface for each addressObject
-#Correlating methods below
-def ipInCidr(ip, cidr):
-    return ipaddress.ip_address(ip) in ipaddress.ip_network(cidr)
-
-def correlateVLANToZone(zoneList, addressVlan):
-    for zone in zoneList:
-        zoneVlans = zone.findall(Interface)
-        for zoneVlan in zoneVlans:
-            zoneVlanNum = zoneVlan.name.split(".")[1]
-            if zoneVlanNum == addressVlan:
-                return zone.name
-    return "NA"
-
-def correlateAddressToVLAN(addressObject, vlanDict):
-    #check if ip is within cidr range of the VLAN IP. (Returned by the getAddressForVLANS method)
-    for vlanNum, vlanIP in vlanDict.items():
-        if ipInCidr(addressObject.value, vlanIP):
-            return vlanNum
-    return "NA"
-
-def correlateAddressToAddressGroup(addressObject, addressGroups):
-    #Unsure if the addressGroup ip is a name or ip
-    for addressGroup in addressGroups:
-        for address in addressGroup.static_value:
-            if addressObject.name == address or addressObject.value == address:
-                return addressGroup.name
-    return "NA"
-
-def getChildrenOfAggInterface(aggInterface):
-    #TODO:Be sure to eventually account for other interface types (Layer2, virtual-wire, tap, ha)
-    subInterfaces = aggInterface.findall(Layer3Subinterface)
-    return subInterfaces
-
-def getAddressForVLANS(subinterfaceList):
-    interfaceMap = {}
-    for subinterface in subinterfaceList:
-        #grab the second half of the interface name, which is the VLANs number
-        vlanNum = subinterface.name.split(".")[1]
-        vlanIP = subinterface.ip
-        interfaceMap[vlanNum] = vlanIP
-    return interfaceMap
-
-main()
+if __name__ == "__main__":
+    main()
